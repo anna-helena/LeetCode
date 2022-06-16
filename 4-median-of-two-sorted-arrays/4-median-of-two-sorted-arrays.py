9746855fe31e0a2722be2d2462ca0d1bbc8a2e98
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-06-17 00:06:03 +0200

    Time: 147 ms (39.80%), Space: 14.2 MB (67.64%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -8,16 +8,12 @@ class Solution:
         N = len(nums1) + len(nums2)
         mn = min(nums1[0] if nums1 else float('inf'), nums2[0] if nums2 else float('inf'))
         mx = max(nums1[-1] if nums1 else float('-inf'), nums2[-1] if nums2 else float('-inf'))
-        print(mn,mx)
         def findNthElement(N):
-            print('in fct')
             lo,hi = mn,mx
             el = lo
             while(lo <= hi):
                 mid = (lo+hi)//2
-                print(mid)
                 res = bisect_right(nums1,mid) + bisect_right(nums2,mid)
-                print(('res',res))
                 if res >= N:
                     el = mid
                     hi = mid - 1
@@ -27,7 +23,5 @@ class Solution:
         
         first_el = findNthElement(N//2+1)
         second_el = findNthElement((N-1)//2+1)
-        print(N//2+1,(N-1)//2+1)
-        print(first_el,second_el)
         return(0.5*(first_el+second_el))
     
