d6f91b16569d8f52ae8460295bafa672c63f98ae
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-06-21 21:54:10 +0200

    Time: 624 ms (17.41%), Space: 25 MB (18.19%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -7,6 +7,4 @@ class Solution:
         if len(nums) <= 4:
 	        return 0
         nums.sort()
-        #Check all possible ways to to remove 3 numbers
-        return min(nums[-1] - nums[3], nums[-2]-nums[2],nums[-3]-nums[1],nums[-4]-nums[0])
-        
+        return min(nums[-4]-nums[0],nums[-3]-nums[1],nums[-2]-nums[2],nums[-1]-nums[3])
