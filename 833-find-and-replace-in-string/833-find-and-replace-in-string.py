d7fbd6eb843f30977e9f81e939e607987b4b8063
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-08-09 22:44:38 +0200

    Time: 46 ms (78.17%), Space: 14 MB (18.95%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -5,22 +5,17 @@ class Solution:
             index = indices[i]
             if (s[index:(index+len(source))] == source):
                 temp[index] = i
-        print(temp)
         until = 0
         new_str = ''
         while (until < len(s)):
             if until in temp:
-                print('until', until)
                 i = temp[until]
                 target = targets[i]
                 new_str = new_str + target
                 until += len(sources[i])
-                print(new_str)
             else:
-                print('until2', until)
                 new_str = new_str + s[until]
                 until += 1
-                print(new_str)
         return new_str
         
             
