fa4f4ca616ff5a0f996441f124823fb1ac56d8e1
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-09-19 00:29:55 +0200

    Time: 92 ms (58.77%), Space: 13.6 MB (89.20%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -5,30 +5,35 @@ class Solution(object):
         :rtype: int
         """
         
-        max_prod_k = nums
+        max_prod_k = nums[0]
         max_min = 1
+        result = max_prod_k
         if nums[0] < 0:
             max_min = nums[0]
         for i,val in enumerate(nums[1:]):
             i += 1
             #print(i, max_min, max_prod_k)
+            temp = val
             if(val == 0):
                 max_min = 1
             elif(val>0):
-                if(max_prod_k[i-1]>0):
-                    max_prod_k[i] = max_prod_k[i-1]*val
+                if(max_prod_k>0):
+                    temp = max_prod_k*val
                     if(max_min<0):
                         max_min*=val
-                elif(max_prod_k[i-1]<0):
+                elif(max_prod_k<0):
                     max_min *=val            
             elif(val<0):
-                max_prod_k[i] = max_min*val
-                if(max_prod_k[i-1]>0):
-                    max_min = val*max_prod_k[i-1]
-                elif(max_prod_k[i-1]<=0):
+                temp = max_min*val
+                if(max_prod_k>0):
+                    max_min = val*max_prod_k
+                elif(max_prod_k<=0):
                     max_min = val
-                
-        return(max(max_prod_k))
+            result = max(result,temp)
+            max_prod_k = temp
+            #max_prod_k[i] = max(val, max_prod_k[i-1]*val, max_min*val)
+            #max_min = 
+        return result
     
             
             
