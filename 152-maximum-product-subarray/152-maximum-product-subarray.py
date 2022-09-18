db4934c2dabafbc4f19ee4e6767b3f0b9e557015
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-09-19 00:32:51 +0200

    Time: 94 ms (55.55%), Space: 13.6 MB (97.30%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -13,8 +13,24 @@ class Solution(object):
         for i,val in enumerate(nums[1:]):
             i += 1
             #print(i, max_min, max_prod_k)
-            temp = max(val, max_prod_k*val, max_min*val)
-            max_min = min(val, max_prod_k*val, max_min*val)
+            temp = val
+            if(val == 0):
+                max_min = 1
+            elif(val>0):
+                if(max_prod_k>0):
+                    temp = max_prod_k*val
+                    if(max_min<0):
+                        max_min*=val
+                elif(max_prod_k<0):
+                    max_min *=val            
+            elif(val<0):
+                temp = max_min*val
+                if(max_prod_k>0):
+                    max_min = val*max_prod_k
+                elif(max_prod_k<=0):
+                    max_min = val
+            #temp = max(val, max_prod_k*val, max_min*val)
+            #max_min = min(val, max_prod_k*val, max_min*val)
             result = max(result,temp)
             max_prod_k = temp
             #max_min = 
