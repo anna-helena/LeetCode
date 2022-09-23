6a2a55f8e050722c8e4ba9da207fcd4824ae3b36
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-09-24 00:02:26 +0200

    Time: 476 ms (67.41%), Space: 15.8 MB (74.81%) - LeetHub

---
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def removeOnes(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
+        #no ones --> ok
+        #entire rows and cols --> ok
+        set_1 = set()
+        set_0 = set()
+        for i,val in enumerate(grid[0]):
+            if val == 0:
+                set_0.add(i)
+            else:
+                set_1.add(i)
+       # print(set_1)
+        #print(set_0)
+        for row_idx in range(1,len(grid)):
+            row = grid[row_idx]
+            set_0_temp = set()
+            set_1_temP = set()
+            for i,val in enumerate(row):
+                if val == 0:
+                    set_0_temp.add(i)
+                else:
+                    set_1_temP.add(i)
+            if((set_0_temp != set_0) & (set_0_temp != set_1)):
+                return False
+        return True
+                
