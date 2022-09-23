1e22054e020399c381e5042ddcc0752ee26e1c69
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-09-24 00:07:53 +0200

    Time: 1087 ms (5.19%), Space: 15.7 MB (86.67%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
@@ -13,8 +13,6 @@ class Solution(object):
                 set_0.add(i)
             else:
                 set_1.add(i)
-       # print(set_1)
-        #print(set_0)
         for row_idx in range(1,len(grid)):
             row = grid[row_idx]
             set_0_temp = set()
