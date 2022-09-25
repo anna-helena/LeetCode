7888c1ad25634674a4b54720ec1bf0e5204c7a02
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-09-25 21:47:14 +0200

    Time: 1988 ms (28.85%), Space: 28.9 MB (60.44%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -4,8 +4,6 @@ from collections import defaultdict
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
         seen = set()
-        prev = {}
-        prev[0] = None
         same = defaultdict(list)
         for idx,val in enumerate(arr):
             same[val].append(idx)
@@ -13,10 +11,8 @@ class Solution:
         q = deque()
         q.append((0,0))
         seen.add(0)
-        result = 0
         while(q):
             curr_idx, dist = q.popleft()
-            print(curr_idx, dist)
             if curr_idx == len(arr)-1:
                 break        
             #add left
@@ -24,14 +20,12 @@ class Solution:
                 l_idx = curr_idx - 1
                 if (l_idx not in seen):
                     q.append((l_idx,dist+1))
-                    prev[l_idx] = curr_idx
                     seen.add(l_idx)
             #add right
             if (curr_idx < len(arr)-1):
                 r_idx = curr_idx + 1
                 if (r_idx not in seen):
                     q.append((r_idx,dist+1))
-                    prev[r_idx] = curr_idx
                     seen.add(r_idx)
             #add same number
             #for new_idx in same[arr[curr_idx]]:
@@ -42,13 +36,8 @@ class Solution:
                     continue
                 if (new_idx not in seen):
                     q.append((new_idx,dist+1))
-                    prev[new_idx] = curr_idx
                     seen.add(new_idx)
                 if new_idx == (len(arr)-1):
                     return (dist + 1)
             del same[arr[curr_idx]]
-        before = prev[curr_idx]
-        while before is not None:
-            result += 1
-            before = prev[before]
         return dist
