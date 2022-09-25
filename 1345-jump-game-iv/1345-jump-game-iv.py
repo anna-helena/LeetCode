e51e781df4c26bd42bca9d206e76bcf7d8465f14
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-09-25 21:44:02 +0200

    Time: 2855 ms (9.33%), Space: 34.1 MB (13.07%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -11,25 +11,26 @@ class Solution:
             same[val].append(idx)
             
         q = deque()
-        q.append(0)
+        q.append((0,0))
         seen.add(0)
         result = 0
         while(q):
-            curr_idx = q.popleft()
+            curr_idx, dist = q.popleft()
+            print(curr_idx, dist)
             if curr_idx == len(arr)-1:
                 break        
             #add left
             if (curr_idx > 0):
                 l_idx = curr_idx - 1
                 if (l_idx not in seen):
-                    q.append(l_idx)
+                    q.append((l_idx,dist+1))
                     prev[l_idx] = curr_idx
                     seen.add(l_idx)
             #add right
             if (curr_idx < len(arr)-1):
                 r_idx = curr_idx + 1
                 if (r_idx not in seen):
-                    q.append(r_idx)
+                    q.append((r_idx,dist+1))
                     prev[r_idx] = curr_idx
                     seen.add(r_idx)
             #add same number
@@ -40,14 +41,14 @@ class Solution:
                 if new_idx == curr_idx:
                     continue
                 if (new_idx not in seen):
-                    q.append(new_idx)
+                    q.append((new_idx,dist+1))
                     prev[new_idx] = curr_idx
                     seen.add(new_idx)
                 if new_idx == (len(arr)-1):
-                    break
+                    return (dist + 1)
             del same[arr[curr_idx]]
         before = prev[curr_idx]
         while before is not None:
             result += 1
             before = prev[before]
-        return result
+        return dist
