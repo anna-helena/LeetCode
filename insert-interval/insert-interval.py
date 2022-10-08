6a00484b4421d3e87eab37b67fb1b74386bdbae7
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-10-08 23:21:34 +0200

    Time: 192 ms, Memory: 17.6 MB - LeetHub

---
+++ insert-interval/insert-interval.py
@@ -0,0 +1,45 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        a,b = newInterval
+        #find binary sect a
+        
+        #biect_left
+        def find_index_higher_equal(val,left=True):
+            if left:
+                i = 0
+            else:
+                i = 1
+            lo = 0
+            hi = len(intervals)
+            while(lo < hi):
+                mid = (lo + hi)//2
+                temp = intervals[mid][i]
+                if val > temp:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            return lo
+        
+        i_a = find_index_higher_equal(a,False)
+        i_b = find_index_higher_equal(b,True)
+        #define new interval and add
+        if intervals:
+            if(i_a<len(intervals)):
+                newInterval[0] = min(a,intervals[i_a][0])
+            else:
+                newInterval[0] = a
+            if (i_b < len(intervals)):
+                if (intervals[i_b][0] == b):
+                    temp_i_b = i_b
+                else:
+                    temp_i_b = i_b-1
+            else:
+                temp_i_b = i_b-1
+            if(temp_i_b>=0):
+                newInterval[1] = max(b,intervals[temp_i_b][1])
+            else:
+                newInterval[1] = b
+        else:
+            temp_i_b = 0
+        
+        return intervals[:i_a] + [newInterval] + intervals[(temp_i_b+1):]
