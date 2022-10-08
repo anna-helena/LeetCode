efbe7615477cee4340abac6e4060283b02f45bc1
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-10-08 23:15:36 +0200

    Time: 159 ms (46.16%), Space: 17.6 MB (5.97%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -43,7 +43,6 @@ class Solution:
                 newInterval[1] = b
         else:
             temp_i_b = 0
-            #del intervals[(i_a):(temp_i_b+1)]
         #intervals.insert(i_a,newInterval)
         
         return intervals[:i_a] + [newInterval] + intervals[(temp_i_b+1):]
