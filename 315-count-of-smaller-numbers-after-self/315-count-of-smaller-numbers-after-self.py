ac400283fc63a918cb3ecaf86981cdd697bf436b
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-10-09 12:36:02 +0200

    Time: 4867 ms (63.55%), Space: 34 MB (69.04%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -7,20 +7,14 @@ class Solution:
         #you how many have been already seen to the right
         #nlogn
         
-        #sorted_dict = {nums[idx]: idx for idx in sorted(range(len(nums)), key=lambda k: nums[k])}
         sorted_indices = sorted(range(len(nums)), key=lambda k: nums[k])
         nums = sorted(nums)
-        print(sorted_indices)
         seen = SortedList()
         result = [0]*len(nums)
         for i,val in enumerate(nums):
             idx = sorted_indices[i]
             #add to seen
             new_idx = seen.bisect_right(idx)
-            if((len(seen) - new_idx)==9):
-                print(seen,new_idx,idx,val)
             result[idx] = len(seen) - new_idx
             seen.add(idx)
-            if((len(seen) - new_idx)==10):
-                print(result)
         return result
