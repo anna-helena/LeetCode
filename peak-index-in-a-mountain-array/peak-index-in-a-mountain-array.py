0f0bff4045eb4ba6eb9201c21bf76f40f5cdcca6
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-10-14 23:23:25 +0200

    Time: 1434 ms, Memory: 27.2 MB - LeetHub

---
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        #adapted binary search 
+        n = len(arr) - 1
+        if n == 2:
+            return 1
+        lo = 1
+        hi = n - 1
+        while(lo <= hi):
+            mid = (lo + hi)//2
+            #if mid < mountain
+            if arr[mid+1]>arr[mid]:
+                lo = mid + 1
+            #if mid > mountain
+            elif arr[mid-1]>arr[mid]:
+                hi = mid - 1
+            #if mid == mountain -> return mid
+            else:
+                return mid
+        return lo
