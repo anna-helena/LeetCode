de4606c9b81dc1adfffcd3c68d6420b9cb8874ef
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-10-22 23:43:18 +0200

    Time: 3268 ms (47.51%), Space: 33.4 MB (17.28%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -3,29 +3,6 @@ from sortedcontainers import SortedDict
 
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        #double missing
-        #[1:2 3:6 4:8 6: 8:]
-        #half missing
-        #[4:2 ]
-        '''
-        1:1,2:1,3:1,4:1,5:1,6:1,8:1
-        [1:2,3:6,4:8,2:4,6:,8:]
-        
-        [4:2,6:3,8:4,2:1]
-        2:2
-        4:2
-        6:1
-        3:1
-        8:1
-        (4,2) -> 2
-        (6,3) -> 1
-        (8,4) -> 1.5
-        (2,1) -> 1.5
-        4,2
-        '''
-        #build two prevs keys to have all possible combination
-        #find set of best combis with adapted DFS
-        #go one by one remember combi as set stop when nr reached, if not ,remove when old one gets two hits (but hits are reduced if one new hits two....)
         if len(changed) % 2 != 0:
             return []
         sorted_ = SortedDict()
