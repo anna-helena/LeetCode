ba0716733415df7a06fd986f5908cbe672f051cc
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-10-28 23:59:58 +0200

    Time: 125 ms (98.21%), Space: 15.2 MB (31.74%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
@@ -1,10 +1,7 @@
 class Solution:
     def numSplits(self, s: str) -> int:
         return self.helper_(s,set(),set())
-        
-        "aac a a ba"
     def helper_(self, rem, l, r):
-        print(rem)
         if len(rem) == 0:
             return 1
         if len(rem) == 1:
@@ -18,8 +15,6 @@ class Solution:
         r_i = len(rem)-1
         l.add(rem[l_i])
         r.add(rem[r_i])      
-        print(l_i,r_i)
-        print(l,r)
         while l_i < len(rem)-2:
             if rem[l_i+1] not in l:
                 break
@@ -29,11 +24,7 @@ class Solution:
             if rem[r_i-1] not in r:
                 break
             r_i -= 1
-        print('after')
-        print(l_i,r_i)
-        print(l,r)
         if l_i >= r_i:
             return l_i-r_i+2 
         return self.helper_(rem[(l_i+1):r_i],l,r)
         
-"acb ad  baada"
