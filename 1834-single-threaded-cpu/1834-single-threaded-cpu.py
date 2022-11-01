5bdf73a8e7c9da6d1d341570d5a93012408be4ab
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-01 20:11:14 +0100

    Time: 2037 ms (92.95%), Space: 63.3 MB (27.25%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -2,10 +2,6 @@ import heapq
 
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        enqueue = []
-        for idx,task in enumerate(tasks):
-            enqueue.append((task[0],idx))
-        heapq.heapify(enqueue)
         enqueue = [(enq,process,idx) for idx, (enq,process) in enumerate(tasks)]
         enqueue.sort()
         time = enqueue[0][0]
