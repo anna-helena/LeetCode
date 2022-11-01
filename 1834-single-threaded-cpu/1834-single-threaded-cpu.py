06b3dc5fe868da6a5430dddf6068bb2f5e7f8e19
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-01 20:12:57 +0100

    Time: 2015 ms (94.19%), Space: 63.3 MB (27.25%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -2,25 +2,25 @@ import heapq
 
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        tasks = [(enq,process,idx) for idx, (enq,process) in enumerate(tasks)]
-        tasks.sort()
-        time = tasks[0][0]
+        enqueue = [(enq,process,idx) for idx, (enq,process) in enumerate(tasks)]
+        enqueue.sort()
+        time = enqueue[0][0]
         consider = []
         sol = []
         idx_start = 0
-        while idx_start < len(tasks):
-            while tasks[idx_start][0] <= time:
-                new_consider = (tasks[idx_start][1],tasks[idx_start][2])
+        while idx_start < len(enqueue):
+            while enqueue[idx_start][0] <= time:
+                new_consider = (enqueue[idx_start][1],enqueue[idx_start][2])
                 heapq.heappush(consider,new_consider)
                 idx_start += 1
-                if idx_start == len(tasks):
+                if idx_start == len(enqueue):
                     break
             if len(consider) > 0:
                 next_consider = heapq.heappop(consider)
                 sol.append(next_consider[1])
                 time += next_consider[0]
             else:
-                time = tasks[idx_start][0]
+                time = enqueue[idx_start][0]
         while consider:
             next_consider = heapq.heappop(consider)
             sol.append(next_consider[1])
