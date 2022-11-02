ef11da43aad70c1778d6818bc9b8cd70487f0106
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-02 16:37:00 +0100

    Time: 252 ms (21.75%), Space: 14 MB (6.85%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for i,row in enumerate(board):
+            prev = set()
+            print(prev)
+            for nr in row:
+                print(prev)
+                if nr == ".":
+                    continue
+                if nr in prev:
+                    return False
+                prev.add(nr)
+        for i in range(9):
+            prev = set()
+            for j in range(9):
+                if board[j][i] == ".":
+                    continue
+                if board[j][i] in prev:
+                    return False
+                prev.add(board[j][i])
+        for i in range(3):
+            for j in range(3):
+                print(prev)
+                prev = set()
+                for i_ in range(3):
+                    for j_ in range(3):
+                        if board[i*3+i_][j*3+j_] == ".":
+                            continue
+                        if board[i*3+i_][j*3+j_] in prev:
+                            return False
+                        prev.add(board[i*3+i_][j*3+j_])
+        return True
+                        
+        
+        
