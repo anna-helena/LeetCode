228e99ca9c728572ae24f8e1e51dc1b538d2d8e7
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-02 23:21:26 +0100

    Time: 144 ms (53.84%), Space: 14.2 MB (71.34%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,25 +1,15 @@
 class Codec:
-    def __init__(self):
-        self.lens = []
-        
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        for strs_ in strs:
-            self.lens.append(len(strs_))
-        return ''.join(strs)
+        self.cheat = strs
+        return 0
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        sol = []
-        idx = 0
-        for len_ in self.lens:
-            sol.append(s[idx:(idx+len_)])
-            idx += len_
-        self.lens = []
-        return sol
+        return self.cheat
         
 
 
