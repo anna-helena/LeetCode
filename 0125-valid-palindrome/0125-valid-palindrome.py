40b9427f663a7a52571be7b4a1f14a0d0bf49dc2
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-03 22:50:08 +0100

    Time: 168 ms (8.27%), Space: 14.6 MB (44.43%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+        nums = set(range(ord('0'),ord('9')+1))
+        letters = set(range(ord('a'),ord('z')+1))
+        letters = letters.union(nums)
+        print(letters)
+        while(i<j):
+            temp_i = s[i]
+            temp_j = s[j]
+            print(temp_i,temp_j)
+            print(ord(temp_i.lower()),ord(temp_j.lower()))
+            if not ord(temp_i.lower()) in letters:
+                i += 1
+                continue
+            if not ord(temp_j.lower()) in letters:
+                j -= 1
+                continue
+            if temp_i.lower() != temp_j.lower():
+                return False
+            i += 1
+            j -= 1
+        return True
