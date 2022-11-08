63a39228802a8f2f57c3f56bcc0b14141ffb2f68
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-09 00:12:11 +0100

    Time: 1673 ms (73.70%), Space: 25 MB (38.60%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,7 +4,9 @@ class Solution:
         curr_max = 0
         for idx in range(1,len(prices)):
             price = prices[idx]
-            minimum = min(minimum,price)
-            curr_max = max(curr_max,price-minimum)
+            if price < minimum:
+                minimum = price
+            else:
+                curr_max = max(curr_max,price-minimum)
         return curr_max
         
