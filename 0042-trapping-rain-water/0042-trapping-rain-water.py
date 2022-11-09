d7081e7ca1222af53c142a48d154b65bef383fe0
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-09 02:31:11 +0100

    Time: 297 ms (46.65%), Space: 15.9 MB (82.27%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -6,32 +6,20 @@ class Solution:
         start_idx = -1
         for idx,h in enumerate(height):
             if h >= start_height:
-                print(start_height,h)
                 for i in range(start_idx+1,idx):
-                    print(i,start_height - height[i])
                     volume += start_height - height[i]
                 start_height = h
                 start_idx = idx
-                print('----')
-        print('start_idx',start_idx)
-        print(len(height)-1)
         if start_idx != len(height)-1:
-            print('test',len(height)-1,start_idx-1)
             start_idx_prev = start_idx
             start_height = 0
             start_idx = len(height)
             for idx in range(len(height)-1,start_idx_prev-1,-1):
-                print(height[idx],idx)
                 if height[idx] >= start_height:
-                    print(start_height,height[idx],idx)
-                    print('loop')
-                    print(start_idx,idx)
                     for i in range(start_idx-1,idx,-1):
-                        print(i,start_height - height[i])
                         volume += start_height - height[i]
                     start_height = height[idx]
                     start_idx = idx
-                    print('----')
         return volume
 
             
