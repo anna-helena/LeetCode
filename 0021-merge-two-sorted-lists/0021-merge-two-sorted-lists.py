39e1025a883e8a854a5e7ddab2b687911ed856c3
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-10 01:32:05 +0100

    Time: 75 ms (43.07%), Space: 14 MB (33.06%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -15,13 +15,5 @@ class Solution:
                 curr.next = list2
                 list2 = list2.next
             curr = curr.next
-        while list1:
-            curr.next = list1
-            list1 = list1.next
-            curr = curr.next
-        
-        while list2:
-            curr.next = list2
-            list2 = list2.next
-            curr = curr.next
+        curr.next = list1 if list1 else list2
         return sol.next
