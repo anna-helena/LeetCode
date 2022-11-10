9710767df478b07e5df486438c9fd5e4c370bad0
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-10 02:10:37 +0100

    Time: 62 ms (95.23%), Space: 14.1 MB (50.41%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,13 +8,9 @@ class Solution:
         idx = 0
         for idx,s_ in enumerate(s):
             if s_ in prev:
-                if prev[s_] < idx_start:
-                    prev[s_] = idx
-                else:
+                if prev[s_] >= idx_start:
                     max_len = max(max_len,idx-idx_start)
                     idx_start = prev[s_]+1
-                    prev[s_] = idx
-            else:
-                prev[s_] = idx
+            prev[s_] = idx
         return max(max_len,idx+1-idx_start)
         
