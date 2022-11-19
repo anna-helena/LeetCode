327e764234703d732e457731fc5b458975b080fb
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-19 17:37:46 +0100

    Time: 881 ms (25.47%), Space: 14.1 MB (7.07%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -5,17 +5,25 @@ class Solution:
         for s1_ in s1:
             first[s1_] += 1
         i = 0
-        while i < len(s2):
+        while i <= (len(s2)-len(s1)):
             if s2[i] not in first:
                 i += 1
                 continue
             temp = defaultdict(int)
+            temp_first = {}
             temp[s2[i]] += 1
+            temp_first[s2[i]] = i
             possible = True
             for j in range(1,len(s1)):
                 if (i+j) < len(s2):
                     if s2[i+j] in first:
                         temp[s2[i+j]] += 1
+                        if temp[s2[i+j]] == 1:
+                            temp_first[s2[i+j]] = i+j
+                        if temp[s2[i+j]] > first[s2[i+j]]:
+                            possible = False
+                            i = temp_first[s2[i+j]] + 1
+                            break
                     else:
                         possible = False
                         i = i + j + 1
