c6e736a32af58485d5fbc299d67e02f85310a751
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-21 00:29:10 +0100

    Time: 30 ms (94.29%), Space: 13.9 MB (57.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,15 @@
 class Solution:
-    def __init__(self):
-        self.seen = {}
     def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return n
-        if n in self.seen:
-            return self.seen[n]
-        one_ = self.climbStairs(n-1) 
-        two_ = self.climbStairs(n-2)
-        self.seen[n] = max(one_,1) + max(two_,1)
-        return max(one_,1) + max(two_,1)
+        i, j = self.climbStairs_helper(n)
+        return i + j
+    
+    #counts distinct ways of climbing stairs for starting by one and starting by two
+    def climbStairs_helper(self, n):
+        if n == 0:
+            return 0, 0
+        if n == 1:
+            return 1, 0
+        prev_i, prev_j = self.climbStairs_helper(n-1)
+        return prev_i + prev_j, prev_i
+        
         
