288724b84779f3e92dc651bf6571710010fb13d6
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-11-21 19:53:26 +0100

    Time: 104 ms (66.53%), Space: 14.5 MB (57.24%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -2,15 +2,14 @@ class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         parents = {}
         res = []
-        for idx,edge in enumerate(edges):
-            x,y = edge
+        for idx,[x,y] in enumerate(edges):
             if x not in parents:
                 parents[x] = x
             if y not in parents:
                 parents[y] = y
             temp = self.find_parent(x,parents)
             if temp == self.find_parent(y,parents):
-                res = edge
+                res = [x,y]
             else:
                 parents[temp] = y
         return res
