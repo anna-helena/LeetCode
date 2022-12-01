0a09a933c6992ffcd26e42a5e2a4f09d56f571af
anna-helena <47527919+anna-helena@users.noreply.github.com>
2022-12-01 14:40:19 +0100

    Time: 415 ms (19.73%), Space: 14.4 MB (84.96%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -15,30 +15,11 @@ class Solution:
                 while s:
                     curr_i,curr_j = s.pop()
                     curr_ar += 1
-                    #up
-                    if curr_i > 0:
-                        if (curr_i-1,curr_j) not in seen:
-                            seen.add((curr_i-1,curr_j))
-                            if grid[curr_i-1][curr_j] == 1:
-                                s.append((curr_i-1,curr_j))
-                    #down
-                    if curr_i < len(grid)-1:
-                        if (curr_i+1,curr_j) not in seen:
-                            seen.add((curr_i+1,curr_j))
-                            if grid[curr_i+1][curr_j] == 1:
-                                s.append((curr_i+1,curr_j))
-                    #left
-                    if curr_j > 0:
-                        if (curr_i,curr_j-1) not in seen:
-                            seen.add((curr_i,curr_j-1))
-                            if grid[curr_i][curr_j-1] == 1:
-                                s.append((curr_i,curr_j-1))
-                    #right
-                    if curr_j < len(grid[0])-1:
-                        if (curr_i,curr_j+1) not in seen:
-                            seen.add((curr_i,curr_j+1))
-                            if grid[curr_i][curr_j+1] == 1:
-                                s.append((curr_i,curr_j+1))
+                    for new_i,new_j in ((curr_i-1,curr_j),(curr_i+1,curr_j),(curr_i,curr_j-1),(curr_i,curr_j+1)):
+                        if (0 <= new_i < len(grid) and 0 <= new_j < len(grid[0]) and (new_i,new_j) not in seen):
+                            seen.add((new_i,new_j))
+                            if grid[new_i][new_j]:
+                                s.append((new_i,new_j))
                 biggest = max(biggest,curr_ar)
         return biggest
                     
